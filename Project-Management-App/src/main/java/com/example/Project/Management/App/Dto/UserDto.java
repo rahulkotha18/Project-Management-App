d7737de2048f3e313c3da069c08a57823b7c57c5
rahul <rahulhopper047@gmail.com>
2019-12-10 12:09:32 +0530

    add Dto

---
+++ Project-Management-App/src/main/java/com/example/Project/Management/App/Dto/UserDto.java
@@ -0,0 +1,22 @@
+package com.example.Project.Management.App.Dto;
+
+public class UserDto {
+    private String name;
+    private String username;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
